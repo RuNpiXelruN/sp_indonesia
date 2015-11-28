cb2939ca2bf422e87a337ea770dfc9972c34870b
runpixelrun <justindavidson23@gmail.com>
2015-11-28 20:13:47 +0000

    changed routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  if Rails.env.production?
+
+      before_filter :authenticate
+
+      protected
+
+      def authenticate
+        authenticate_or_request_with_http_basic do |username, password|
+          username == "jd2015" && password == "jd2015"
+        end
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   
   get '/testnav', to: 'pages#testnav'
   
+  root 'pages#gifpage'
+  
   
   
   # The priority is based upon order of creation: first created -> highest priority.
