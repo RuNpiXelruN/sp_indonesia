516af87ae69dc8f389656b823d184c83c8d69e36
runpixelrun <justindavidson23@gmail.com>
2016-01-12 12:33:57 +0000

    Added partner form to homepage, complete with styling and jquery transitions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,93 @@
 //= require materialize-sprockets
 //= require_tree .
 
+
+
+$(document).ready(function() {
+    
+    $(".partner-button").click(function(){
+        $(this).hide();
+        $('.contact-form').css({'height':'40vw', 'margin': '10vw 9.5vw 10vw'});
+        $('.contact-form').fadeIn(300);
+        $('.form-content').delay().fadeIn(300);
+
+        $('html, body').scrollTo('.scroll-here',{duration:'1000', offsetTop : '0'});
+    });
+    
+    $('.scroll-here').click(function() {
+        $(this).hide();
+        $('.partner-button').show();
+        $('.contact-form').css({'height': '0vw', 'margin': '0'});
+        $('.contact-form').fadeOut(300);
+        $('.form-content').delay().fadeOut(300);
+        
+        $('html, body').scrollTo('.partners-callout-section', {duration: '500', offsetTop: '10'});
+    });
+    
+});
+
+//  GREAT FOR USE WITH MEDIA QUERIES!! 
+
+// $(document).ready(function() {
+//     // run test on initial page load
+//     checkSize();
+
+//     // run test on resize of the window
+//     $(window).resize(checkSize);
+// });
+
+// //Function to the css rule
+// function checkSize(){
+//     if ($(".sampleClass").css("float") == "none" ){
+//         // your code here
+//     }
+// }
+
+
+
+
+
+// $('html, body').animate({
+//     scrollTop: $(window).scrollTop() + 200
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
     
     // var hidden = true;
