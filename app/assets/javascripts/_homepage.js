58c91dfde8e57ad85af9bcb6a99de08d2c72674b
runpixelrun <justindavidson23@gmail.com>
2016-01-13 11:24:00 +0000

    Added styles first @media to homepage and rebuilt partners and australia section for entire site

---
+++ app/assets/javascripts/_homepage.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+    $('.partner-area').click(function() {
+        $(this).next('.partner-offices').slideToggle(500);
+    });
+});
+
+
+
+
+
+
+
+// partner-area
+
+// partner-offices
