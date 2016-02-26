5770fb7ba1eb966f7dbde1c39364f2dc746f59fd
runpixelrun <justindavidson23@gmail.com>
2016-02-26 04:55:35 +0000

    Added @media's in sweet alert css file for alert styling and scaling

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -5,15 +5,18 @@
 
 $(document).ready(function() {
     $(".contact-form").on("ajax:success", function() {
-        swal("Good job!", "You clicked the button!", "success");
+        swal({   title: "Sweet!",   text: "Here's a custom image.",   imageUrl: "assets/Social_Playground_logo_Circle-compressor.png" });
+        // sweetAlert("Good job!", "You clicked the button!", "success");
         $(this).slideUp();
         $(this).prev('.form-close').click();
+        $('button.confirm').css('background-color','rgb(140, 212, 245) !important');
         $('.contact-form input').val('');
         $('.contact-form textarea').val('');
     }); 
 
     $(".contact-form").on("ajax:error", function(xhr, status, error) {
         sweetAlert("Oops...", "Something went wrong!", "error");
+        $('button.confirm').css('background-color','#ff9b7e');
         console.log(error);
     });
 });
